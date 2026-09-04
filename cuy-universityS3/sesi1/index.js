c0b7e9eafd9068078ceabe5eb6e55f38d269fa80
AWP22nd <aaawwwppp22@gmail.com>
2026-09-04 21:42:09 +0700

    Initialize Express server in index.js
    Set up a basic Express server that responds with 'Hello World!'

--- cuy-universityS3/sesi1/index.js
+++ cuy-universityS3/sesi1/index.js
@@ -1,2 +1,16 @@
-const data = "testing . . ."
-console.log(data)
+// const data = "testing . . ."
+// console.log(data)
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// Run it with "npm run api-service", then open in localhost:port in your web browser
