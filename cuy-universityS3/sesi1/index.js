c8ca9c9a88d24c3f3e1d5f35da8cb2e6e46aba83
AWP22nd <aaawwwppp22@gmail.com>
2026-09-06 11:09:08 +0700

    Update index.js

--- cuy-universityS3/sesi1/index.js
+++ cuy-universityS3/sesi1/index.js
@@ -4,8 +4,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 // main routes or URL or endpoint Method GET
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Main Page');
@@ -15,16 +18,17 @@ app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/login', (req, res) => {
+    console.log({ requestFromOutside : req.body });
+    res.send('Login Success');
+});
 
-// app.post('/login', (req, res) => {
-//   if (req.name === 'ske') {
-//     res.send('Login Success');
-//   }
-// });
-
+app.put('/username', (req, res) => {
+  console.log( { updatedData : req.body } );
+  res.send('Username updated');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
 // Run it with "npm run api-service", then open in localhost:port in your web browser
