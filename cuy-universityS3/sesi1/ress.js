50a0f11997d72e674557a489ea16347515396e6a
AWP22nd <aaawwwppp22@gmail.com>
2026-09-17 21:47:26 +0700

    Update ress.js

--- cuy-universityS3/sesi1/ress.js
+++ cuy-universityS3/sesi1/ress.js
@@ -2,9 +2,9 @@ const response = (statusCode, data, message, res) => {
     res.status(statusCode).json({
         payload: {
             status_code: statusCode,
-            data: data,
-            message: message
+            data: data
         },
+        message: message,
         pagination: {
             prev: "",
             next: "",
