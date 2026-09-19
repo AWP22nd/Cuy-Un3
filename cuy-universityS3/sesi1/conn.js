f4c6f647d97d0456bede44792e79541ab900bdbd
AWP22nd <aaawwwppp22@gmail.com>
2026-09-19 09:36:59 +0700

    Update conn.js

--- cuy-universityS3/sesi1/conn.js
+++ cuy-universityS3/sesi1/conn.js
@@ -4,7 +4,17 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'cuy_university'
+    database: 'cuy_university',
+    port: 3307,
+    socketPath: '/opt/lampp/var/mysql/mysql.sock'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.message);
+        return;
+    }
+    console.log('Database connected successfully');
 });
 
 module.exports = db;
